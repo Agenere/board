94b88ac1673144e8a83665434e9cd93c01e7f8a1
Administrator <Administrator@smart20102>
2019-02-15 12:22:33 +0900

    [김동열] board 삭제시 get방식인데 Controller에서 post 방식으로 받는다고 해서 안된것 수정

--- springboard/src/main/java/cafe/jjdev/springboard/controller/BoardController.java
+++ springboard/src/main/java/cafe/jjdev/springboard/controller/BoardController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +18,7 @@ public class BoardController {
     @Autowired private BoardService boardService;
     
      // 글 삭제 요청
-    @PostMapping(value="/boardDelete")
+    @GetMapping(value="/boardDelete")
     public String boardRemove(int boardNo) {
     	boardService.removeBoard(boardNo);
         return "redirect:/boardList";
